95f67feb06422d741bf4619c43de28a2ae0d438f
Jabed Akhtar <64619782+Jabed-Akhtar@users.noreply.github.com>
2025-02-26 21:38:01 +0100

    Delete catkin_ws/build directory

--- catkin_ws/build/vehicle_auto/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samir/Samir/Projects/vehicle_auto_ai_ros_ws/catkin_ws/src/vehicle_auto
-# Build directory: /home/samir/Samir/Projects/vehicle_auto_ai_ros_ws/catkin_ws/build/vehicle_auto
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
